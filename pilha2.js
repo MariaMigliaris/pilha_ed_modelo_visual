5c71a32887180bf34370890baa58a87e6a08c29b
Maria Eduarda <mariaeduardamigliaris2@gmail.com>
2024-06-02 23:31:48 -0300

    projeto atualizado

--- pilha2.js
+++ pilha2.js
@@ -1,88 +1,73 @@
-function setarCampo(){
-	formPilha.elemento.value = "";
-	formPilha.elemento.focus();
+class Pilha {
+  constructor() {
+    this.qtdElementos = 0;
+    this.formPilha = {
+      pilha: document.getElementById('conteudo')
+    };
+  }
+
+  estouroPilha(tipo) {
+    if (tipo === 'push' && this.qtdElementos === 11) {
+      alert('A pilha está cheia!!!');
+      return false;
+    }
+    if (tipo === 'pop' && this.qtdElementos === 0) {
+      alert('A pilha está vazia!!!');
+      return false;
+    }
+    return true;
+  }
+
+  pushElemento() {
+    if (this.estouroPilha('push')) {
+      if (this.qtdElementos < 11) {
+        const toco = document.createElement('div');
+        toco.classList.add('toco');
+        toco.style.transform = `rotate(${this.qtdElementos * 15}deg)`; // Rotaciona o toco de acordo com a posição na pilha
+        this.formPilha.pilha.appendChild(toco);
+        this.qtdElementos += 1;
+      }
+    }
+    this.mostrarPilha();
+  }
+
+  popElemento() {
+    if (this.estouroPilha('pop')) {
+      if (this.qtdElementos > 0) {
+        this.formPilha.pilha.removeChild(this.formPilha.pilha.lastChild);
+        this.qtdElementos -= 1;
+      }
+    }
+    this.mostrarPilha();
+  }
+
+  mostrarPilha() {
+    const fogo = document.createElement('div');
+    fogo.classList.add('fogo');
+
+    while (this.formPilha.pilha.firstChild) {
+      this.formPilha.pilha.removeChild(this.formPilha.pilha.firstChild);
+    }
+
+    for (let i = 0; i < this.qtdElementos; i++) {
+      const toco = document.createElement('div');
+      toco.classList.add('toco');
+      this.formPilha.pilha.appendChild(toco);
+    }
+
+    if (this.qtdElementos === 11) {
+      
+      this.formPilha.pilha.appendChild(fogo);
+    }
+  }
 }
 
-var qtdElementos = 0;
+const pilha = new Pilha();
 
-function estouro_pilha(qtd, tipo){
-	if ((tipo == 'push') && (qtd == 10)){
-		alert('A pilha está cheia!!!');
-		return false;
-	}
-	if ((tipo == 'pop') && (qtd == 0)){
-		alert('A pilha está vazia!!!');
-		return false;
-	}
-	return true;
-}
+document.getElementById('pushButton').addEventListener('click', () => {
+  pilha.pushElemento();
+});
 
-function verificaPontoVirgula(elemento){
-	var pos = elemento.indexOf(';');
-	if (pos != -1) {
-		alert("O valor possui ; ");
-		return false;
-	}
-	return true;
-}
-
-function pushElemento(elemento){
-	if (estouro_pilha(qtdElementos, 'push')){
-		if (verificaPontoVirgula(elemento)){
-			if (qtdElementos == 0) {
-				formPilha.pilha.value = elemento
-			}else{
-				formPilha.pilha.value += ";"+elemento;
-			}
-			qtdElementos += 1;
-		}
-	}
-	mostrarPilha();	
-}
-
-function popElemento(){
-	if (estouro_pilha(qtdElementos, 'pop')){
-		var str = formPilha.pilha.value;
-		var	pilha = str.split(";");
-		
-		formPilha.pilha.value = " ";
-		for (index = 0; index < pilha.length - 1; index++){
-			if (index == 0){
-				formPilha.pilha.value = pilha[index];
-			}else{
-				formPilha.pilha.value += ";"+pilha[index];
-			}
-		}
-		qtdElementos -= 1;
-	}
-	mostrarPilha();
-}
-
-function mostrarPilha(){
-	var str = formPilha.pilha.value;
-	var retorno = "A quantidade de elementos da pilha é: "+qtdElementos;
-	retorno += "<br><table width='500' border='0' class='tabela'>";
-	retorno += "<tr><td width='104'>Posi&ccedil;&atilde;o</td>";
-	for (i = 1; i <= 10; i++){
-		retorno += "<td width='35'><div align='center'><strong>"+i+"</strong></div></td>";
-	}
-	retorno += "</tr><tr><td>Elemento</td>";
-	
-	if (str != ""){
-		var pilha = str.split(";");
-		if (qtdElementos > 0){
-			for (index = 0; index < pilha.length; index++) {
-				posicao = index + 1;
-				retorno += "<td><div align='center'>";
-				retorno += ""+pilha[index]+"</div></td>";
-			}
-			while (posicao < 10){
-				retorno += "<td><div align='center'></div></td>";
-				posicao += 1;
-			}
-		}
-		retorno += "</tr></table>";
-		document.getElementById("conteudo").innerHTML = retorno;
-	}			
-	setarCampo();
-}
+document.getElementById('popButton').addEventListener('click', () => {
+  pilha.popElemento();
+});
